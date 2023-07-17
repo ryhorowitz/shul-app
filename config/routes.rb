52e1c3570d082b3ae9ae9536bae4313f7309a0be
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-07-17 15:09:41 -0400

    edited syntax error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources Users, only: [:create, :show]
+  resources :users, only: [:create, :show]
   
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
